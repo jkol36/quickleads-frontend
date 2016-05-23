efb9073273d2abe142a54d5f8bb1d531eb3b8348
Jon Kolman <jonathankolman@gmail.com>
2016-05-23 12:29:27 -0400

    initial

---
+++ webpack.config.dev.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE,
+    'babel-polyfill',
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve:{
+    root: [
+      path.resolve('./src')
+    ]
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }, {
+      test: /\.less$/,
+      loader: "style-loader!css-loader!less-loader"
+    },
+    {
+      test: /\.json$/,
+      loader: 'json'
+    }],
+    preLoaders: [
+      {
+        test: /\.js?$/,
+        loaders: ['eslint-loader'],
+        include: path.join(__dirname, 'src'),
+        exclude: /bundle\.js$/
+      }
+    ]
+  },
+  eslint: {
+    configFile: '.eslintrc'
+  },
+  stats: {
+    colors: true
+  }
+};
